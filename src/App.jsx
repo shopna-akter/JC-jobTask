c4796dacaae25bc6b3b41f85b3c5a681f5ea8a70
shopna akter <shopna88akter@gmail.com>
2024-08-17 21:53:48 +0600

    Undo Redo left

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import { useState } from 'react'
-
+import './App.css'
 function App() {
   const [count, setCount] = useState(0)
-
+  const handleSubstract = () => {
+    if (0 >= count) {
+      alert('minimum number is reached')
+    }
+    else {
+      setCount(count - 1)
+    }
+  }
+  const handleIncrease = () => {
+    if (count >= 150) {
+      alert('maximum number is reached')
+    }
+    else {
+      setCount(count + 1)
+    }
+  }
   return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className='flex min-h-screen flex-col justify-center items-center'>
+      <div>
+        <div className='flex flex-col justify-center items-center'>
+          <label>{count}</label>
+        </div>
+        <div className='flex'>
+          <h2 className='mr-2'>0</h2>
+          <progress className="progress progress-bar w-56 mt-2" value={count} max="150"></progress>
+          <h2 className='ml-2'>150</h2>
+        </div>
       </div>
-    </>
+      <div className="flex">
+        <button onClick={handleSubstract} className='btn btn-ghost'>Substract</button>
+        <button onClick={handleIncrease} className='btn btn-ghost'>Increase</button>
+      </div>
+    </div>
   )
 }
 
